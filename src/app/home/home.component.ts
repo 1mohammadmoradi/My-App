ebbb9cde34ae3ecad93f556ac296ba20a936bad1
mohammad <mohammad_1msh@yahoo.com>
2022-04-23 18:38:09 +0430

    fixing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,16 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+ 
+  flage =true;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+ngOnInit(): void {
+  console.log('flage',this.flage);
+}
   
   headers = ['Id','FirstName','LastName','City'];
-  rows = [{Id : "1" , FirstName :"Mohammad" , LastName : "Moradi" , City : "Sisakht" } ,
-  {Id : "2" , FirstName :"Ali" , LastName : "Hekmati" , City : "Shahreza" }
+  rows = [
+  {Id : "1" , FirstName :"Mohammad" , LastName : "Moradi" , City : "Sisakht" } ,
+  {Id : "2" , FirstName :"Liana" , LastName : "Moradi" , City : "Tehran" },
+  {Id : "3" , FirstName :"Delsa" , LastName : "Salehi" , City : "Sisakht" },
+  {Id : "4" , FirstName :"Ali" , LastName : "Salehi" , City : "Tehran" },
 
 
 ]
+
+ngAfterInit() : void {
+
+
+  console.log('flage',this.flage);
+}
+
 }

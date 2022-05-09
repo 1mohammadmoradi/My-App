77867861d56db3123099877565e7098ade570434
mohammad <mohammad_1msh@yahoo.com>
2022-05-10 00:51:15 +0430

    fixing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { ModalDismissReasons, NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -9,15 +10,51 @@ export class AppComponent {
   
   
   flage= false;
-  title = 'My-App4';
-  constructor( ) {
+  //title = 'My-App4';
+  // constructor( ) {
      
-  }
+  // }
+  title = 'ng-bootstrap-modal-demo';
+  closeResult: string;
+  modalOptions:NgbModalOptions;
+  
   click(){
     
      this.flage=!this.flage;
      console.log('ssss',this.flage);
      return this.flage;
    }
+
+
+   /////
+
+ 
+   constructor(
+     private modalService: NgbModal
+   ){
+     this.modalOptions = {
+       backdrop:'static',
+       backdropClass:'customBackdrop'
+     }
+   }
+   
+   open(content) {
+     this.modalService.open(content, this.modalOptions).result.then((result) => {
+       this.closeResult = `Closed with: ${result}`;
+     }, (reason) => {
+       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+     });
+   }
+ 
+   private getDismissReason(reason: any): string {
+     if (reason === ModalDismissReasons.ESC) {
+       return 'by pressing ESC';
+     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+       return 'by clicking on a backdrop';
+     } else {
+       return  `with: ${reason}`;
+     }
+   }
+
   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +15,9 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [HomeComponent],
   bootstrap: [AppComponent]

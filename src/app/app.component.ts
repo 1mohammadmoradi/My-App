8548a1297884bb364e320c9d5c33c18356f17b5f
mohammad <mohammad_1msh@yahoo.com>
2022-07-08 16:08:11 +0430

    fixing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,22 +39,25 @@ export class AppComponent {
    }
    
    open(content) {
-     this.modalService.open(content, this.modalOptions).result.then((result) => {
-       this.closeResult = `Closed with: ${result}`;
-     }, (reason) => {
-       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-     });
+     this.modalService.open(content)
+    //  .then((result) => {
+    //    //this.closeResult = `Closed with: ${result}`;
+    //  },
+    //   (reason) => {
+    //    this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    //  }
+     //);
    }
  
-   private getDismissReason(reason: any): string {
-     if (reason === ModalDismissReasons.ESC) {
-       return 'by pressing ESC';
-     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-       return 'by clicking on a backdrop';
-     } else {
-       return  `with: ${reason}`;
-     }
-   }
+  //  private getDismissReason(reason: any): string {
+  //    if (reason === ModalDismissReasons.ESC) {
+  //      return 'by pressing ESC';
+  //    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+  //      return 'by clicking on a backdrop';
+  //    } else {
+  //      return  `with: ${reason}`;
+  //    }
+  //  }
 
   
 }
